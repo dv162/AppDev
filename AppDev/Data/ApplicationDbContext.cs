2d28dd5c933058bdbd242efd0c1a0adb14e69f92
QuocTrungdv <107343030+dv162@users.noreply.github.com>
2024-03-21 11:56:37 +0700

    edit

---
+++ AppDev/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AppDev.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppDev.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Adventure", Description = "So funny" },
+            new Category { Id = 2, Name = "Action", Description = "So awsome" },
+            new Category { Id = 3, Name = "Horror", Description = "So scared" }
+            );
+        }
+    }
+}
